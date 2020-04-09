d4ef6271a53ca778bbb278ade59439002a3fdcf1
manish465 <ms5978307@gmail.com>
2020-04-09 14:30:28 -0700

    used middlewere

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 
+app.use("/post", () => {
+    console.log("we are useing middlewere");
+});
+
 app.get("/", (req, res) => {
     res.send("HOME");
+});
+
+app.get("/post", (req, res) => {
+    res.send("POST");
 });
 
 app.listen(5000);
